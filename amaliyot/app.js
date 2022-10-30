25ee1f8c574e916a4c18a1d07c9794a53e5c15d0
bexzodxamdamov <bexzodxamdamov5@gmail.com>
2022-10-30 18:04:47 +0500

    first commit

---
+++ amaliyot/app.js
@@ -0,0 +1,63 @@
+// No_1
+
+// let a = prompt("pulizzi kritin");
+
+// if (a <= 500) {
+//   console.log("chorvoga borin");
+// } else if (a >= 1000) {
+//   console.log("dubaiga borin");
+// }
+
+// No_2
+
+// let b = prompt("son krinin");
+
+// if (a % 3 == 0) {
+//   console.log("fizz");
+// } else if (a % 5 == 0) {
+//   console.log("bazz");
+// } else if (a % 3 == 0 && a % 5 == 0) {
+//   console.log("fizz - bazz");
+// }
+
+//No_3
+
+// let yosh = prompt("yoshizzi tanlen");
+
+// if (18 < yosh) {
+//   alert("xush kelibsizz");
+// }
+
+// No_4(1)
+
+// let d = 2.1;
+
+// console.log(Math.floor(d));
+// bu qolduli sonni masalan 2,1 bosa uni 2 ga korintirib beradi
+
+// console.log(Math.ceil(d));
+// bu qolduli sonni masalan 2,1 bosa uni 3 ga korintirib beradi
+
+// No_4(2)
+
+// let e = 2.111111;
+
+// console.log(d.toFixed(2));
+// bu qolduli sonni masalan 2,11111 bosa uni oxirgi 2 ta sonni
+// korintirib beradi
+
+//  No_5
+
+// let f = 6;
+
+// if ((f = 6)) {
+//   console.log(Math.floor(Math.random() * 10));
+// }
+
+// No_6
+
+// let g = 10;
+
+// if (g % 2 === 0) {
+//   console.log("son juft");
+// }
---
+++ homework/main2.js
@@ -0,0 +1,15 @@
+// task 1
+// Input ___ Output
+// 5 ; 8 ___ true
+// 4 ; 8 ___ false
+// 3 ; 7 ___ true
+
+// let a = prompt("son kritin");
+
+// if (((a = 5), 8)) {
+//   console.log("true");
+// } else if (((a = 4), 8)) {
+//   console.log("false");
+// } else if (((a = 3), 7)) {
+//   console.log("true");
+// }
